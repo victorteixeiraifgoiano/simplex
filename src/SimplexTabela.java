5b468feab9435145f6414f615805f7adbff7cebf
victor <victor.teixeira@estudante.ifgoiano.edu.br>
2023-10-10 20:41:38 -0300

    .

--- src/SimplexTabela.java
+++ src/SimplexTabela.java
@@ -75,6 +75,88 @@ public class SimplexTabela {
         }
     }
 
+    public int findEnteringVariable() {
+        int numVariables = numColumns - 1;
+        double[] coefficients = new double[numVariables];
+    
+        // Calcule os coeficientes da função objetivo
+        for (int j = 0; j < numVariables; j++) {
+            coefficients[j] = tableau[0][j];
+        }
+    
+        // Encontre a coluna de entrada (variável candidata)
+        int enteringVariable = -1;
+        for (int j = 0; j < numVariables; j++) {
+            if (coefficients[j] > 0) {
+                enteringVariable = j;
+                break;
+            }
+        }
+    
+        return enteringVariable;
+    }
+    
+    public int findLeavingVariable(int pivotColumn) {
+        int pivotRow = -1;
+        double minRatio = Double.MAX_VALUE;
+    
+        for (int i = 1; i < numRows; i++) {
+            double rhs = getEntry(i, numColumns - 1);
+            double coefficient = getEntry(i, pivotColumn);
+    
+            if (coefficient > 0) {
+                double ratio = rhs / coefficient;
+    
+                if (ratio < minRatio) {
+                    minRatio = ratio;
+                    pivotRow = i;
+                }
+            }
+        }
+    
+        return pivotRow;
+    }
+
+    public void performPivotOperation(int pivotRow, int pivotColumn) {
+        double pivotElement = tableau[pivotRow][pivotColumn];
+        int numRows = tableau.length;
+        int numColumns = tableau[0].length;
+    
+        // Fazer o elemento pivot igual a 1
+        for (int j = 0; j < numColumns; j++) {
+            tableau[pivotRow][j] /= pivotElement;
+        }
+    
+        // Fazer os elementos na coluna do pivô (exceto o pivô) igual a zero
+        for (int i = 0; i < numRows; i++) {
+            if (i != pivotRow) {
+                double factor = tableau[i][pivotColumn];
+                for (int j = 0; j < numColumns; j++) {
+                    tableau[i][j] -= factor * tableau[pivotRow][j];
+                }
+            }
+        }
+    }
+    
+
+    public void simplexAlgorithm() {
+        while (true) {
+            int pivotColumn = findEnteringVariable();
+            if (pivotColumn == -1) {
+                // Todas as entradas na linha da função objetivo são não positivas, pare
+                break;
+            }
+
+            int pivotRow = findLeavingVariable(pivotColumn);
+            if (pivotRow == -1) {
+                // Não há saída válida, o problema é ilimitado, pare
+                break;
+            }
+
+            performPivotOperation(pivotRow, pivotColumn);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -116,6 +198,36 @@ public class SimplexTabela {
         // Encontre as variáveis básicas e não básicas
         tableau.findBasicAndNonBasicVariables();
 
+        // Chame o algoritmo simplex
+        tableau.simplexAlgorithm();
+
+        //
+        //
+        //
+// Mostre o resultado final das variáveis e da função objetivo
+System.out.println("\nResultado Final das Variáveis:");
+for (int j = 0; j < numVariables; j++) {
+    boolean isBasic = false;
+    for (int i = 1; i < numRows; i++) {
+        if (tableau.getEntry(i, j) == 1) {
+            isBasic = true;
+            System.out.println("x" + (j + 1) + " = " + tableau.getEntry(i, numColumns - 1));
+            break;
+        }
+    }
+    if (!isBasic) {
+        System.out.println("x" + (j + 1) + " = 0");
+    }
+}
+
+System.out.println("\nResultado Final da Função Objetivo:");
+System.out.println("Z = " + tableau.getEntry(0, numColumns - 1));
+        //
+        //
+        //
+
+
         scanner.close();
     }
+
 }
